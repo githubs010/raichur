144d746e03bc28c859d36a410d649fb1b1b6893e
githubs010 <github01012@gmail.com>
2026-05-15 19:26:05 +0530

    v2

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-var API_URL = "https://script.google.com/macros/s/AKfycbyQ8CFYtDYPv3oHRI9h-SpywDlzUkbcc7XuZNV7EySTUt6_16WCkL1KAbYbM80rIv8Dfg/exec";
+var API_URL = "https://script.google.com/macros/s/AKfycby0hu0AkxQ83FOrWw330HwAzAOa7kFGLOQUOzZY3RM_X4EEHFU591VQ1GVpjfoQqh2I/exec";
 function apiGet(action, cb) { fetch(API_URL + "?action=" + action).then(r=>r.text()).then(t=>cb(null, JSON.parse(t).data||[])).catch(e=>cb(e.toString(),[])); }
 function apiPost(body, cb) { fetch(API_URL, {method:"POST", headers:{"Content-Type":"text/plain;charset=utf-8"}, body:JSON.stringify(body)}).then(r=>r.text()).then(t=>cb(null, JSON.parse(t))).catch(e=>cb(e.toString(),{})); }
 function badge(s) { return "<span class='badge' style='background:"+({"Pending":"#da7101","Assigned":"#006494","In Progress":"#a12c7b","Completed":"#437a22","Open":"#a12c7b","Resolved":"#437a22"}[s]||"#888")+"'>"+s+"</span>"; }
